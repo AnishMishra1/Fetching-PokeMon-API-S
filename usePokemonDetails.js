399047c812a61fc13d0f061d776725b0d2e40535
AnishMishra1 <124522187+AnishMishra1@users.noreply.github.com>
2023-10-09 16:00:45 +0530

    Add files via upload

---
+++ usePokemonDetails.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+const usePokemonDetails = (id ,pokemonName) => {
+
+    
+    const [pokemon, setPokemon] = useState({});
+
+    async function downloadPokemon() {
+    
+       try {
+
+              let response;
+            if(pokemonName) {
+                response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+            } else {
+                response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+            }
+            const pokemonOfSameTypes = await axios.get(`https://pokeapi.co/api/v2/type/${response.data.types ? response.data.types[0].type.name : ''}`);
+            setPokemon({
+                name: response.data.name,
+                image: response.data.sprites.other.dream_world.front_default,
+                weight: response.data.weight,
+                height: response.data.height,
+                types: response.data.types.map((t) => t.type.name),
+                similarPokemons: pokemonOfSameTypes.data.pokemon
+            });
+            setPokemonListState({...pokemonListState, type: response.data.types ? response.data.types[0].type.name : '' })
+        
+        
+       } catch (error) {
+        console.log("Something Went Wrong");
+      }
+
+  
+    }
+    const [ pokemonListState, setPokemonListState]= useState({});
+        
+
+
+    useEffect(() => {
+        downloadPokemon()
+    },[])
+
+  return [pokemon];
+}
+
+export default usePokemonDetails
