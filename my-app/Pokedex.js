c608ffc2e37ecd94bce4d22d7a8e2f5adbfa75b4
AnishMishra1 <124522187+AnishMishra1@users.noreply.github.com>
2023-10-09 15:14:01 +0530

    Add files via upload

---
+++ my-app/Pokedex.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import PokenmonList from "../PokemonList/PokenmonList.js";
+import Search from "../Search/Search.js"
+import PokemonDetails from "../PokemonDetails/PokemonDetails.js";
+
+//css import
+import './Pokedex.css' 
+
+
+
+function Pokedex() {
+   const [searchTerm, setSearchTerm] = useState('')
+
+    return (
+      <div className="pokemon-wrapper">
+    
+      <Search  updateSearchTerm = {setSearchTerm} />
+      
+      {(!searchTerm) ?<PokenmonList />: <PokemonDetails key={searchTerm} pokemonName= {searchTerm}/>}
+      </div>
+
+    )
+}
+
+export default Pokedex;
